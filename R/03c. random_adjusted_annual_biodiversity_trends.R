c28aea22082d9e95accce5336a943abb034432aa
Joemillard <joeymillard@hotmail.co.uk>
2020-05-14 23:51:37 +0100

    add bivariate plot for annual

--- R/03c. random_adjusted_annual_biodiversity_trends.R
+++ R/03c. random_adjusted_annual_biodiversity_trends.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(lme4)
 library(boot)
 library(RColorBrewer)
+library(cowplot)
 
 # source the functions R script
 source("R/00. functions.R")
@@ -112,7 +113,7 @@ create_lpi <- function(lambdas, ind = 1:nrow(lambdas)) {
 run_each_group <- function(lambda_files, random_trend){
   
   # Bootstrap these to get confidence intervals
-  dbi.boot <- boot(lambda_files, create_lpi, R = 1000)
+  dbi.boot <- boot(lambda_files, create_lpi, R = 100)
   
   # Construct dataframe and get mean and 95% intervals
   boot_res <- data.frame(LPI = dbi.boot$t0)
@@ -156,3 +157,125 @@ fin_bound_trends %>%
 
 ggsave("random_annual_adjusted_class_SAI_free.png", scale = 1.3, dpi = 350)
 
+
+# figure for overall changes of different groupings
+# first calculate average lambda for each series
+language_frame <- fin_bound_trends %>%
+  group_by(language, taxa) %>%
+  mutate(lambda = c(0, diff(log10(LPI)))) %>%
+  mutate(conf_diff = 1 - (mean(LPI_upr-LPI_lwr))) %>% 
+  mutate(average_lambda = mean(lambda)) %>% 
+  ungroup() %>% 
+  filter(taxa != "random") %>%
+  select(language, taxa, conf_diff, average_lambda) %>%
+  unique() %>%
+  mutate(factor_rate = factor(ifelse(average_lambda > 0, "increasing", "decreasing"))) %>% 
+  mutate(factor_conf = factor(ifelse(conf_diff > quantile(conf_diff, 0.5), "high", "low")))
+
+# calculate number of factors for rates and confidence for each language
+sort_rate_lang <- language_frame %>%
+  group_by(language) %>%
+  count(factor_rate) %>%
+  ungroup() %>%
+  filter(factor_rate == "increasing") %>%
+  select(language, n)
+
+# add en at the bottom for engligh with no low confidence groupings
+sort_conf_lang <- language_frame %>%
+  group_by(language) %>%
+  count(factor_conf) %>%
+  ungroup()  %>%
+  filter(factor_conf == "low") %>%
+  select(language, n)
+
+# join together number of factors and sort on rate and confidence
+joined_order_lang <- inner_join(sort_rate_lang, sort_conf_lang, by = "language") %>%
+  arrange(desc(n.x), n.y)  %>% 
+  mutate(language = factor(language, levels = language)) %>% pull(language)
+
+# calculate number of factors for rates and confidence for each taxa
+sort_rate_taxa <- language_frame %>%
+  group_by(taxa) %>%
+  count(factor_rate) %>%
+  filter(factor_rate == "decreasing") %>%
+  select(taxa, n) %>%
+  arrange(desc(n)) %>%
+  ungroup()
+
+sort_conf_taxa <- language_frame %>%
+  group_by(taxa) %>%
+  count(factor_conf) %>%
+  filter(factor_conf == "low") %>%
+  select(taxa, n) %>%
+  arrange(n) %>%
+  ungroup()
+
+# join together number of factors and sort on rate and confidence
+joined_order_taxa <- inner_join(sort_rate_taxa, sort_conf_taxa, by = "taxa") %>%
+  arrange(desc(n.x), n.y)  %>% 
+  mutate(taxa = factor(taxa, levels = taxa)) %>% pull(taxa)
+
+# build plot for language and certainty 
+rate_plot <- fin_bound_trends %>%
+  group_by(language, taxa) %>%
+  mutate(lambda = c(0, diff(log10(LPI)))) %>% 
+  filter(lambda != 0) %>%
+  mutate(conf_diff = 1 - (mean(LPI_upr-LPI_lwr))) %>% 
+  mutate(mean_lambda = mean(lambda)) %>%
+  ungroup() %>%
+  select(mean_lambda, taxa, language, conf_diff) %>%
+  unique() %>%
+  filter(taxa != "random") %>% 
+  mutate(factor_rate = factor(ifelse(mean_lambda > 0, "increasing", "decreasing"))) %>% 
+  mutate(factor_conf = factor(ifelse(conf_diff > quantile(conf_diff, 0.5), "high", "low"))) %>%
+  mutate(factor_rate = factor(factor_rate, levels = c("increasing", "decreasing"), labels = c("Increase", "Decrease"))) %>%
+  mutate(factor_conf = factor(factor_conf, levels = c("low", "high"), labels = c("Low",  "High"))) %>%
+  mutate(alpha_val = as.numeric(factor_conf)) %>%
+  #mutate(taxa = factor(taxa, levels = joined_order_taxa, 
+  #                     labels = c("Insecta", "Actinopterygii", "Amphibia", "Mammalia", "Aves", "Reptilia"))) %>% 
+  #mutate(language = factor(language, levels = joined_order_lang,
+  #                         labels = c("Arabic", "French", "Chinese", "English", "German", "Spanish", "Italian", "Japanese", "Portuguese", "Russian"))) %>%
+  mutate(plot_order = (as.numeric(factor_rate) + as.numeric(factor_conf))) %>%
+  ggplot() +
+  geom_tile(aes(x = language, y = taxa, fill = factor_rate, alpha = alpha_val), colour = "white", size = 1.5) +
+  scale_fill_manual("Rate of change", values = c("#009E73", "#D55E00")) +
+  scale_alpha(range = c(0.5, 1)) +
+  theme_bw() +
+  theme(panel.grid = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.title = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.border = element_blank(),
+        legend.position = "none") 
+
+# build dataframe for setting out the legend
+d <- expand.grid(x=1:2,y=1:2)
+d <- merge(d,data.frame(x=1:2,xlabel=c("X low", "X high")),by="x")
+d <- merge(d,data.frame(y=1:2,ylabel=c("Y low", "Y high")),by="y")
+d$alpha_val <- d$x
+
+# build the legend with the appropriate colours for change and certainty
+g.legend <- ggplot(d, aes(x,y,fill=ylabel, alpha = alpha_val))+
+  geom_tile(colour = "white", size = 1.5) +
+  scale_fill_manual("Rate of change", values = c("#009E73", "#D55E00")) +
+  scale_alpha(range = c(0.5, 1)) +
+  scale_x_continuous(breaks = c(1, 2), labels = c("Low", "High")) +
+  geom_segment(aes(x=0.3, xend = 0.3, y = 1.4, yend = 0.6 ), arrow = arrow(length = unit(0.15, "cm"), type = "closed")) +
+  geom_segment(aes(x=0.3, xend = 0.3, y = 1.6, yend = 2.4), arrow = arrow(length = unit(0.15, "cm"), type = "closed")) +
+  theme_void() +
+  theme(legend.position="none",
+        panel.background=element_blank(),
+        plot.margin=margin(t=10,b=10,l=10),
+        axis.title.y = element_text(angle = 90),
+        axis.title.x = element_text(hjust = 0.6, vjust = -0.8),
+        axis.text.x = element_text()) +
+  labs(title=NULL,x= "Certainty", y = "Change") +
+  theme(axis.title=element_text(color="black"))
+
+# combine the plot and the legend
+ggdraw() +
+  draw_plot(rate_plot, -0.13, width = 1, height = 1, scale = 0.75) +
+  draw_plot(g.legend, width = 0.2, height = 0.3, scale = 0.95, hjust = -3.65, vjust = -1.96)
+
+# save the plot with legend and plot combined for change and certainty
+ggsave("average_change_annual_uncertainty.png", scale = 1.1, dpi = 400)
