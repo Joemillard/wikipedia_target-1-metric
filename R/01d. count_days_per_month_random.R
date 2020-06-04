706064b5893f0d6ff4aad39aede3812c679ad0da
Joemillard <joeymillard@hotmail.co.uk>
2020-06-04 11:07:06 +0100

    commit script for smoothing days per month

--- R/01d. count_days_per_month_random.R
+++ R/01d. count_days_per_month_random.R
@@ -4,6 +4,7 @@ library(data.table)
 library(forcats)
 library(ggplot2)
 library(parallel)
+library(mgcv)
 
 # read in additional functions
 source("R/00. functions.R")
@@ -103,3 +104,82 @@ for(i in 1:length(language_views_monthly)){
 
 # save as rds file to rds
 saveRDS(days_all, "J:/submission_2/random_days_per_month_rate.rds")
+
+## smmooth the rate of change for the index of days with loess regression
+rate_change <- list(list())
+rate_change[[1]] <- readRDS("Z:/submission_2/random_days_per_month_rate.rds")
+
+# set up three empty lists
+language_bound <- list()
+
+# iterate through through each langauge, then each class, and then each article for each class/language
+for(i in 1:length(rate_change)){
+  print(i)
+  bound_subframes <- list()
+  for(j in 1:length(rate_change[[i]])){
+    article_subframe <- list()
+    
+    # select for only articles represented across the whole series (57 months)
+    rate_change[[i]][[j]] <- select_comp(rate_change[[i]][[j]])
+    
+    # select all the unique articles for that class/language group
+    unique_articles <- unique(rate_change[[i]][[j]]$q_wikidata)
+    print(j)
+    
+    # iterate through all the articles of that class/language
+    for(k in 1:length(unique_articles)){
+      
+      # filter for each unique article, create a variable for the length of the series
+      article_subframe[[k]] <- rate_change[[i]][[j]] %>%
+        filter(q_wikidata == unique_articles[k])
+      
+      # create vector for length of the x data, and jitter slightly for those articles represented with similar values
+      x_range <- 1:length(article_subframe[[k]]$date_col)
+      #jittered_n <- jitter(article_subframe[[k]]$n, factor = 0.1)
+      
+      # run a loess regression to smooth the values for each article, and assigned to a smoothed values column
+      y.loess <- gam(article_subframe[[k]]$n~s(x_range))
+      article_subframe[[k]]$smoothed_values <- predict(y.loess, data.frame(x_range))
+      
+    }
+    
+    # bind together the article dataframes from that class/language combination
+    bound_subframes[[j]] <- rbindlist(article_subframe)
+    
+  }
+  
+  # stack together the class/language dataframes into list
+  language_bound[[i]] <- bound_subframes
+}
+
+# convert the smoothed values into a rate of change, and then convert that into an index
+calculate_index <- function(data_file){
+  data_fin <- data_file %>%
+    group_by(article) %>%
+    mutate(smoothed_rate = c(0, diff(log10(abs(smoothed_values))))) %>%
+    mutate(rate = c(0, diff(log10(abs(n))))) %>%
+    mutate(smoothed_index = cumprod(10^c(smoothed_rate))) %>%
+    mutate(index = cumprod(10^c(rate))) %>%
+    #filter(smoothed_values < 0) %>%
+    ungroup() %>%
+    mutate(article = as.factor(article))
+  
+  
+  return(data_fin)
+}
+
+# run the function for converting each set of smoothed values into an index
+smoothed_day_rate <- list()
+for(i in 1:length(rate_change)){
+  smoothed_day_rate[[i]] <- lapply(language_bound[[i]], calculate_index)
+}
+
+# plot one smoothed index as an example
+smoothed_day_rate[[1]][[9]] %>%
+  filter(q_wikidata == "5004851") %>% 
+  ggplot() +
+  geom_line(aes(x = date_col, y = index), colour = "red") +
+  geom_line(aes(x = date_col, y = smoothed_index), colour = "black")
+
+# save the new smoothed trends
+saveRDS(smoothed_day_rate, "smoothed_day_rate_random.rds")
