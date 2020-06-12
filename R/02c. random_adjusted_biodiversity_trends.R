c991cf0f9f4b82ec3130491be0153e511dcf36eb
Joemillard <joeymillard@hotmail.co.uk>
2020-06-12 16:57:33 +0100

    revert back to version without day adjustment for average daily views

--- R/02c. random_adjusted_biodiversity_trends.R
+++ R/02c. random_adjusted_biodiversity_trends.R
@@ -14,57 +14,39 @@ source("R/00. functions.R")
 
 # script for pollinator models using new language data
 # read in the random rds file
-directory <- "Z:/submission_2/lambda_files"
+directory <- "Z:/submission_2/daily_average_lambda/"
 
 # read in the string of languages - original order sorted alphabetically for files read in
 languages <- c("\\^es_", "\\^fr_", "\\^de_", "\\^ja_", "\\^it_", "\\^ar_", "\\^ru_", "\\^pt_", "\\^zh_", "\\^en_")
 
 # read in the lambda files 
-random_trend <- readRDS("Z:/submission_2/overall_10-random-languages.rds")
+random_trend <- readRDS("Z:/submission_2/overall_daily-views_10-random-languages.rds")
 
-# read in the days per month trends - random needs to be assigned to a list to fit with species functions
-random_days <- list(list())
-random_days[[1]] <- readRDS("Z:/submission_2/random_days_per_month_rate.rds")
-species_days <- readRDS("Z:/submission_2/species_days_per_month_rate.rds")
-
-# read in the rds for total monthly views and random monthly views to retrieve the lambda ids
-total_monthly_views <- readRDS("Z:/submission_2/total_monthly_views_10-languages.rds")
-random_monthly_views <- list(list())
-random_monthly_views[[1]] <- readRDS("Z:/submission_2/total_monthly_views_random_10-languages.rds") # - random needs to be assigned to a list to fit with species functions
-
-# selet columns to identify each article, and then unique
-restructure_views <- function(x){
-  data_fin <- x %>%
-    select(article, q_wikidata, dec_date, av_views) %>%
-    mutate(SpeciesSSet = as.character(as.numeric(as.factor(article)))) %>%
-    filter(complete.cases(.)) %>%
-    select(q_wikidata, SpeciesSSet, article) %>%
-    unique() %>%
-    mutate(SpeciesSSet = as.character(SpeciesSSet))
-  return(data_fin)
-}
-
-# rescale each dataframe to start at 1970 and merge back with the views, subset for complete series, then output lpi structure with original id
-rescale_monthly_views <- function(monthly_views){
-  iucn_views_poll <- list()
+# adjust each of the lambda values for random
+# adjust the year column
+for(i in 1:length(random_trend)){
+  random_trend[[i]]$date <- as.numeric(rownames(random_trend[[i]]))
+  random_trend[[i]]$Year <- (random_trend[[i]]$date - 1970)/12 + 2015
+  random_trend[[i]]$Year <- as.character(random_trend[[i]]$Year)
   
-  # iterate through each class/language combination, and then run functions to format view data
-  for(i in 1:length(monthly_views)){
-    print(length(monthly_views))
-    iucn_inner_list <-list()
-    for(j in 1:length(monthly_views[[i]])){
-      print(length(monthly_views[[i]]))
-      iucn_inner_list[[j]] <- rescale_iucn(monthly_views[[i]][[j]])
-      iucn_inner_list[[j]] <- select_comp(iucn_inner_list[[j]]) # select time series length
-      iucn_inner_list[[j]] <- restructure_views(iucn_inner_list[[j]])
-    }
-    iucn_views_poll[[i]] <- iucn_inner_list
-  }
-  return(iucn_views_poll)
+  # calculate lambda for random
+  random_trend[[i]] <- random_trend[[i]] %>%
+    filter(date %in% c(1977:2033))
+  random_trend[[i]]$lamda = c(0, diff(log10(random_trend[[i]]$LPI_final[1:57])))
+  random_trend[[i]]$date <- paste("X", random_trend[[i]]$date, sep = "")
+  random_trend[[i]]$language <- languages[i]
 }
+
+# bind together and plot the random trends
+rbindlist(random_trend) %>%
+  ggplot() +
+  geom_line(aes(x = Year, y = LPI_final, group = language)) +
+  geom_ribbon(aes(x = Year, ymin = CI_low, ymax = CI_high, group = language), alpha = 0.3) +
+  facet_wrap(~language) +
+  theme_bw()
 
 # string for pollinating classes, plus random
-classes <- c("actinopterygii", "amphibia", "aves", "insecta", "mammalia", "reptilia")
+classes <- c("actinopterygii", "amphibia", "aves", "insecta", "mammalia", "reptilia", "random")
 
 # read in the view data for all taxonomic classes
 # loop through each directory and create a list of all files for users
@@ -101,122 +83,19 @@ user_files <- view_directories(classes,
 # read in all the files in groups for each language
 language_views <- list()
 system.time(for(i in 1:length(user_files)){
-  language_views[[i]] <- lapply(user_files[[i]], fread, encoding = "UTF-8", stringsAsFactors = FALSE, colClasses = list(character = "SpeciesSSet"))
+  language_views[[i]] <- lapply(user_files[[i]], fread, encoding = "UTF-8", stringsAsFactors = FALSE)
 })
 
-## adjust the species trends and the random trends for the number of day per month trends
-# merge each lambda file with the speciesSSet ID from view data
-merge_monthly_days <- function(lambda_files, monthly_views, view_type){
-  merge_lambda <- list()
-  merge_fin_lambda <- list()
-  
-  # due to difference in nesting between random and species, need to specify view set type
-  if(view_type == "species"){
-    for(i in 1:length(lambda_files)){
-      for(j in 1:length(lambda_files[[i]])){
-        merge_lambda[[j]] <- inner_join(lambda_files[[i]][[j]], monthly_views[[j]][[i]], by = "SpeciesSSet") %>%
-          mutate(taxa = classes[i]) %>% # assign class as column
-          mutate(language = languages[j]) # merge each set of lambda files with the q_wikidata and add columns for class and language
-        print(nrow(lambda_files[[i]][[j]]) - nrow(merge_lambda[[j]])) # print row subtraction to check dataframes are same size
-      }
-      merge_fin_lambda[[i]] <- merge_lambda
-    }
+# adjust the lambdas for each species for each language with random
+adj_lambdas <- list()
+all_lambdas <- list()
+for(i in 1:length(language_views)){
+  for(j in 1:length(random_trend)){
+    data_file <- language_views[[i]][[j]]
+    adj_lambdas[[j]] <- cbind(data_file[, 1:3], sweep(data_file[, 4:ncol(data_file)], 2, random_trend[[j]]$lamda, FUN = "-"))
   }
-  
-  # if type is not 'species' (random), need to switch i and j for monthly views
-  else{
-    for(i in 1:length(lambda_files)){
-      for(j in 1:length(lambda_files[[i]])){
-        merge_lambda[[j]] <- inner_join(lambda_files[[i]][[j]], monthly_views[[i]][[j]], by = "SpeciesSSet") %>%
-          mutate(language = languages[j]) # merge each set of lambda files with the q_wikidata and add columns for class and language
-        print(nrow(lambda_files[[i]][[j]]) - nrow(merge_lambda[[j]])) # print row subtraction to check dataframes are same size
-      }
-      merge_fin_lambda[[i]] <- merge_lambda
-    }
-  }
-  return(merge_fin_lambda)
+  all_lambdas[[i]] <- adj_lambdas
 }
-
-# adjust each set of lambdas for the rate of change of days for that article
-adjust_day_rate <- function(lambda_files, day_rate, view_type){
-  whole_day_adjusted <- list()
-  if(view_type == "species"){
-    for(i in 1:length(lambda_files)){
-      total_lambda_merge <- list()
-        for(j in 1:length(lambda_files[[i]])){
-          total_lambda_merge[[j]] <- reshape2::melt(lambda_files[[i]][[j]], id = c("V1", "SpeciesSSet", "Freq", "q_wikidata", "article", "language", "taxa")) %>%
-            mutate(variable = as.character((variable)))
-          day_rate[[j]][[i]]$dec_date <- as.character(day_rate[[j]][[i]]$dec_date)
-          total_lambda_merge[[j]] <- inner_join(total_lambda_merge[[j]], day_rate[[j]][[i]], by = c("q_wikidata", "article", "variable" = "dec_date"))
-          total_lambda_merge[[j]]$adjusted_rate <- total_lambda_merge[[j]]$value - total_lambda_merge[[j]]$rate
-        }
-      whole_day_adjusted[[i]] <- total_lambda_merge
-    }
-  }
-  else{
-    for(i in 1:length(lambda_files)){
-      total_lambda_merge <- list()
-      for(j in 1:length(lambda_files[[i]])){
-        total_lambda_merge[[j]] <- reshape2::melt(lambda_files[[i]][[j]], id = c("V1", "SpeciesSSet", "Freq", "q_wikidata", "article", "language")) %>%
-          mutate(variable = as.character((variable)))
-        day_rate[[i]][[j]]$dec_date <- as.character(day_rate[[i]][[j]]$dec_date)
-        total_lambda_merge[[j]] <- inner_join(total_lambda_merge[[j]], day_rate[[i]][[j]], by = c("q_wikidata", "article", "variable" = "dec_date"))
-        total_lambda_merge[[j]]$adjusted_rate <- total_lambda_merge[[j]]$value - total_lambda_merge[[j]]$rate
-      }
-      whole_day_adjusted[[i]] <- total_lambda_merge
-    }
-  }
-  return(whole_day_adjusted)
-}
-
-# recast data adjusted for number of days into lambda file format of next step
-recast_lambda <- function(adjusted_lambda){
-  for(i in 1:length(adjusted_lambda)){
-    for(j in 1:length(adjusted_lambda[[i]])){
-      adjusted_lambda[[i]][[j]] <- adjusted_lambda[[i]][[j]] %>% 
-        select(V1, SpeciesSSet, Freq, variable, adjusted_rate)
-      adjusted_lambda[[i]][[j]] <- reshape2::dcast(adjusted_lambda[[i]][[j]], V1 + SpeciesSSet + Freq ~ variable, value.var = "adjusted_rate")
-    }
-  }
-  return(adjusted_lambda)
-}
-
-merge_fin_lambda <- adjust_day_rate(lambda_files = merge_monthly_days(lambda_files = language_views, 
-                                                                      monthly_views = rescale_monthly_views(total_monthly_views),
-                                                                      view_type = "species"), 
-                                    day_rate = species_days,
-                                    view_type = "species") %>% recast_lambda()
-
-#merge_fin_lambda <- merge_monthly_days(lambda_files = language_views, 
-#                                       monthly_views = rescale_monthly_views(total_monthly_views),
-#                                       view_type = "species")
-
-#for(i in 1:length(merge_fin_lambda)){
-#  for(j in 1:length(merge_fin_lambda[[i]])){
-#    merge_fin_lambda[[i]][[j]] <- merge_fin_lambda[[i]][[j]] %>% select(-q_wikidata, -article, -language, -taxa)
-#  }
-#}
-
-# read in all the lambda files for the random data for each language
-random_views <- list(list())
-random_directories <- view_directories(classes = "random", directory)
-system.time(for(i in 1:length(random_directories[[1]])){
-  random_views[[1]][[i]] <- fread(random_directories[[1]][i], encoding = "UTF-8", stringsAsFactors = FALSE, colClasses = list(character = "SpeciesSSet"))
-})
-
-random_fin_lambda <- adjust_day_rate(lambda_files = merge_monthly_days(lambda_files = random_views, 
-                                                                       monthly_views = rescale_monthly_views(random_monthly_views),
-                                                                       view_type = "random"), 
-                                     day_rate = random_days,
-                                     view_type = "random") %>% recast_lambda()
-
-#random_fin_lambda <- merge_monthly_days(lambda_files = random_views, 
-#                                        monthly_views = rescale_monthly_views(random_monthly_views),
-#                                        view_type = "random")
-
-#for(i in 1:length(random_fin_lambda[[1]])){
-#  random_fin_lambda[[1]][[i]] <- random_fin_lambda[[1]][[i]] %>% select(-q_wikidata, -article, -language)
-#}
 
 # Function to calculate index from lambdas selected by 'ind'
 create_lpi <- function(lambdas, ind = 1:nrow(lambdas)) {
@@ -233,53 +112,17 @@ create_lpi <- function(lambdas, ind = 1:nrow(lambdas)) {
 }
 
 # function for boostrapping the create_lpi function for each lambda, and generating a 95 % confidence interval
-run_each_group <- function(lambda_files, random_trends_adjusted){
+run_each_group <- function(lambda_files, random_trend){
   
   # Bootstrap these to get confidence intervals
-  dbi.boot <- boot(lambda_files, create_lpi, R = 50)
+  dbi.boot <- boot(lambda_files, create_lpi, R = 10)
   
   # Construct dataframe and get mean and 95% intervals
   boot_res <- data.frame(LPI = dbi.boot$t0)
-  boot_res$Year <- random_trends_adjusted$Year[1:(nrow(random_trends_adjusted))]
+  boot_res$Year <- random_trend$Year[1:(nrow(random_trend))]
   boot_res$LPI_upr <- apply(dbi.boot$t, 2, quantile, probs = c(0.975), na.rm = TRUE) 
   boot_res$LPI_lwr <- apply(dbi.boot$t, 2, quantile, probs = c(0.025), na.rm = TRUE)
   return(boot_res)
-}
-
-# run the boostrapping of trends for each lambda, and adjust for the random of that language
-random_trends_adjusted <- list()
-for(i in 1:length(random_fin_lambda[[1]])){
-  random_trends_adjusted[[i]] <- run_each_group(random_fin_lambda[[1]][[i]], random_trend[[i]]) %>%
-      mutate(language = languages[i])
-}
-
-# adjust each of the lambda values for random
-# adjust the year column
-for(i in 1:length(random_trends_adjusted)){
-  random_trends_adjusted[[i]]$date <- as.numeric(1977:2033)
-  random_trends_adjusted[[i]]$Year <- (random_trends_adjusted[[i]]$date - 1970)/12 + 2015
-  random_trends_adjusted[[i]]$Year <- as.character(random_trends_adjusted[[i]]$Year)
-  random_trends_adjusted[[i]]$lamda <- c(0, diff(log10(random_trends_adjusted[[i]]$LPI[1:57])))
-  random_trends_adjusted[[i]]$date <- paste("X", random_trends_adjusted[[i]]$date, sep = "")
-}
-
-# bind together and plot the random trends
-rbindlist(random_trends_adjusted) %>%
-  ggplot() +
-  geom_line(aes(x = Year, y = LPI, group = language)) +
-  geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr, group = language), alpha = 0.3) +
-  facet_wrap(~language) +
-  theme_bw()
-
-# adjust the lambdas for each species for each language with random
-adj_lambdas <- list()
-all_lambdas <- list()
-for(i in 1:length(merge_fin_lambda)){
-  for(j in 1:length(merge_fin_lambda[[i]])){
-    data_file <- merge_fin_lambda[[i]][[j]]
-    adj_lambdas[[j]] <- cbind(data_file[, 1:3], sweep(data_file[, 4:ncol(data_file)], 2, random_trends_adjusted[[j]]$lamda, FUN = "-"))
-  }
-  all_lambdas[[i]] <- adj_lambdas
 }
 
 # run the boostrapping of trends for each lambda, and adjust for the random of that language
@@ -287,8 +130,8 @@ lpi_trends_adjusted <- list()
 bound_trends <- list()
 for(i in 1:length(all_lambdas)){
   for(j in 1:length(all_lambdas[[i]])){
-    lpi_trends_adjusted[[j]] <- run_each_group(all_lambdas[[i]][[j]], random_trends_adjusted[[j]]) %>%
-      mutate(language = random_trends_adjusted[[j]]$language)
+    lpi_trends_adjusted[[j]] <- run_each_group(all_lambdas[[i]][[j]], random_trend[[j]]) %>%
+      mutate(language = random_trend[[j]]$language)
     
   }
   
@@ -304,17 +147,17 @@ fin_bound_trends <- rbindlist(bound_trends)
 fin_bound_trends %>%
   mutate(Year = as.numeric(Year)) %>%
   ggplot() +
-  #geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr, fill = language), alpha = 0.3) +
-  geom_smooth(aes(x = Year, y = LPI, colour = language), span  = 0.3) +
+  geom_ribbon(aes(x = Year, ymin = LPI_lwr, ymax = LPI_upr, fill = language), alpha = 0.3) +
+  geom_line(aes(x = Year, y = LPI, colour = language)) +
   geom_hline(yintercept = 1, linetype = "dashed", size = 1) +
   scale_fill_brewer(palette="Paired") +
   scale_colour_brewer(palette="Paired") +
-  facet_wrap(~taxa) +
+  facet_wrap(~taxa, scales = "free_y") +
   ylab("SAI") +
   xlab(NULL) +
   theme_bw()
 
-ggsave("random-adjusted_day-adjusted__class_SAI_free_1000_95.png", scale = 1.3, dpi = 350)
+ggsave("random_adjusted_class_SAI_free_1000_95.png", scale = 1.3, dpi = 350)
 
 # convert series back to lambda, and then take sets varying the start date up by one
 # figure for overall changes of different groupings
@@ -354,10 +197,10 @@ series_start_var <- series_start_var %>%
 # build plot for language and certainty 
 rate_plot_series_var <- series_start_var %>%
   mutate(up_down = factor(up_down, levels = c("increasing", "decreasing"), labels = c("Increase", "Decrease"))) %>%
-  #mutate(taxa = factor(taxa, levels = c("insecta", "actinopterygii", "amphibia", "mammalia", "aves", "reptilia"),
-  #                     labels = c("Insecta", "Actinopterygii", "Amphibia", "Mammalia", "Aves", "Reptilia"))) %>% 
-  #mutate(language = factor(language, levels = c("\\^ar_", "\\^fr_", "\\^zh_", "\\^en_", "\\^de_", "\\^es_", "\\^it_", "\\^ja_", "\\^pt_" , "\\^ru_"),
-  #                        labels = c("Arabic", "French", "Chinese", "English", "German", "Spanish", "Italian", "Japanese", "Portuguese", "Russian"))) %>%
+  mutate(taxa = factor(taxa, levels = c("insecta", "actinopterygii", "amphibia", "mammalia", "aves", "reptilia"),
+                       labels = c("Insecta", "Actinopterygii", "Amphibia", "Mammalia", "Aves", "Reptilia"))) %>% 
+  mutate(language = factor(language, levels = c("\\^ar_", "\\^fr_", "\\^zh_", "\\^en_", "\\^de_", "\\^es_", "\\^it_", "\\^ja_", "\\^pt_" , "\\^ru_"),
+                           labels = c("Arabic", "French", "Chinese", "English", "German", "Spanish", "Italian", "Japanese", "Portuguese", "Russian"))) %>%
   ggplot() +
   ggtitle("Total monthly view trends") +
   geom_tile(aes(x = language, y = taxa, fill = up_down), colour = "white", size = 1.5) +
