2997db25b610eac37f79f6512d36148ea4c64d48
Joemillard <joeymillard@hotmail.co.uk>
2020-06-12 19:49:01 +0100

    build the overall random trend from the bootstrap approach elsewhere

--- R/02b. random_language_trends.R
+++ R/02b. random_language_trends.R
@@ -5,16 +5,17 @@
 library(ggplot2)
 library(rlpi)
 library(dplyr)
-#library(data.table)
+library(data.table)
+library(boot)
 
 # read in additional functions
 source("R/00. functions.R")
 
 # set up vector for languages, classes, and directory
-languages <- c("^es_", "^fr_", "^de_", "^ja_", "^it_", "^ar_", "^ru_", "^pt_", "^zh_", "^en_")
+languages <- c("\\^es_", "\\^fr_", "\\^de_", "\\^ja_", "\\^it_", "\\^ar_", "\\^ru_", "\\^pt_", "\\^zh_", "\\^en_")
 
 # read in the rds for total monthly views
-total_monthly_views <- readRDS("Z:submission_2/average_daily_views_random_10-languages.rds")
+total_monthly_views <- readRDS("Z:/submission_2/average_daily_views_random_10-languages.rds")
 
 ## format for the lpi function
 # rescale each dataframe to start at 1970 and merge back with the views
@@ -38,4 +39,105 @@ for(i in 1:length(languages)){
   lpi_trends[[i]] <- LPIMain(paste(languages[i], "random_all_infile_conf.txt", sep = "_"), REF_YEAR = 1977, PLOT_MAX = 2033, goParallel = TRUE)
 }
 
+# save the rds file for the year values
 saveRDS(lpi_trends, "Z:/submission_2/overall_daily-views_10-random-languages.rds")
+random_trend <- readRDS("Z:/submission_2/overall_daily-views_10-random-languages.rds")
+
+# adjust each of the lambda values for random
+# adjust the year column
+for(i in 1:length(random_trend)){
+  random_trend[[i]]$date <- as.numeric(rownames(random_trend[[i]]))
+  random_trend[[i]]$Year <- (random_trend[[i]]$date - 1970)/12 + 2015
+  random_trend[[i]]$Year <- as.character(random_trend[[i]]$Year)
+  
+  # calculate lambda for random
+  random_trend[[i]] <- random_trend[[i]] %>%
+    filter(date %in% c(1977:2033))
+  random_trend[[i]]$lamda = c(0, diff(log10(random_trend[[i]]$LPI_final[1:57])))
+  random_trend[[i]]$date <- paste("X", random_trend[[i]]$date, sep = "")
+  random_trend[[i]]$language <- languages[i]
+}
+
+# read in the view data for all taxonomic classes
+# loop through each directory and create a list of all files for users
+view_directories <- function(classes, directory){
+  
+  # bring in all the files in that directory and assign to a list
+  view_files <- list()
+  for(i in 1:length(languages)){
+    view_files[[i]] <- list.files(directory, pattern = languages[i])
+  }
+  
+  # unlist the files in the correct order
+  file_order <- unlist(view_files)
+  
+  # set up empty list for files for each language
+  user_files_dir <- list()
+  user_files <- list()
+  
+  # set up each of the file directories and order consisten with the random overall trend
+  for(i in 1:length(classes)){
+    user_files[[i]] <- list.files(directory, pattern = classes[i])
+    user_files[[i]] <- user_files[[i]][order(match(user_files[[i]], file_order))]
+    user_files_dir[[i]] <- paste0(directory, "/", user_files[[i]])
+  }
+  
+  # return list of full file paths for each language
+  return(user_files_dir)
+}
+
+# Function to calculate index from lambdas selected by 'ind'
+create_lpi <- function(lambdas, ind = 1:nrow(lambdas)) {
+  
+  # remove na rows
+  lambdas_new <- lambdas[complete.cases(lambdas), ]
+  
+  # select columns from lambda file to calculate mean, and build a cumprod trend
+  lambda_data <- lambdas_new[, 5:ncol(lambdas_new)]
+  this_lambdas <- lambda_data[ind, ]
+  mean_ann_lambda <- colMeans(this_lambdas, na.rm = TRUE)
+  trend <- cumprod(10^c(0, mean_ann_lambda))
+  return(trend)
+}
+
+# function for boostrapping the create_lpi function for each lambda, and generating a 95 % confidence interval
+run_each_group <- function(lambda_files, random_trend){
+  
+  # Bootstrap these to get confidence intervals
+  dbi.boot <- boot(lambda_files, create_lpi, R = 1000)
+  
+  # Construct dataframe and get mean and 95% intervals
+  boot_res <- data.frame(LPI = dbi.boot$t0)
+  boot_res$Year <- random_trend$Year[1:(nrow(random_trend))]
+  boot_res$LPI_upr <- apply(dbi.boot$t, 2, quantile, probs = c(0.975), na.rm = TRUE) 
+  boot_res$LPI_lwr <- apply(dbi.boot$t, 2, quantile, probs = c(0.025), na.rm = TRUE)
+  return(boot_res)
+}
+
+## read in the lambda files to build the average random trend for each language
+# run the function with 10 languages, specifying the directory
+user_files <- view_directories(classes = "random",
+                               directory = here::here("data/class_wiki_indices/submission_2/lambda_files/average_lambda"))
+
+# read in all the files in groups for each language
+language_views <- list()
+system.time(for(i in 1:length(user_files)){
+  language_views[[i]] <- lapply(user_files[[i]], fread, encoding = "UTF-8", stringsAsFactors = FALSE)
+})
+
+# run the boostrapping of trends for each lambda, and adjust for the random of that language
+lpi_trends_adjusted <- list()
+bound_trends <- list()
+for(i in 1:length(language_views[[1]])){
+    lpi_trends_adjusted[[i]] <- run_each_group(language_views[[1]][[i]], random_trend[[i]]) %>%
+      mutate(language = random_trend[[i]]$language) %>%
+      select(LPI, LPI_lwr, LPI_upr) %>%
+      rename("LPI_final" = "LPI") %>%
+      rename("CI_low" = "LPI_lwr") %>%
+      rename("CI_high" = "LPI_upr")
+    
+    rownames(lpi_trends_adjusted[[i]]) <- 1977:2033
+}
+  
+# resave the average lambda for random views, according to bootstrap method used throughout
+saveRDS(lpi_trends_adjusted, "Z:/submission_2/overall_daily-views_10-random-languages_from_lambda.rds")
