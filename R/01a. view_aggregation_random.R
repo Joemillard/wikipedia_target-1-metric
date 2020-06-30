c8e4ac730196b3e1ae39cb00d8d20734c927ef09
Joemillard <joeymillard@hotmail.co.uk>
2020-06-30 14:49:16 +0100

    random views with species removed

--- R/01a. view_aggregation_random.R
+++ R/01a. view_aggregation_random.R
@@ -162,5 +162,70 @@ for(i in 1:length(language_views_monthly)){
 
 # save total monthly views as an rds
 saveRDS(language_views_monthly_bound, "Z:/submission_2/total_monthly_views_random_10-languages.rds")
-
 saveRDS(language_views_monthly_bound, "Z:/submission_2/average_daily_views_random_10-languages.rds")
+
+# read back in the total monthly views - note need to remove those appearing in the species trends removed
+# removing pages occuring in random trend
+total_random_views <- readRDS("Z:/submission_2/total_views/total_monthly_views_random_10-languages.rds")
+
+# set up vectors of wiki project class to remove any animal species from the random data
+wiki_proj <- paste(c("es", "fr", "de", "ja", "it", "ar", "ru", "pt", "zh", "en"), "wiki", sep = "")
+taxa_groups <- c("ACTINOPTERYGII", "AMPHIBIA", "AVES", "INSECTA", "MAMMALIA", "REPTILIA")
+
+# read in the biodiversity pages
+biodiversity_pages <- read.csv(here::here("data/class_wiki_indices/submission_2/all_iucn_titles.csv"), encoding = "UTF-8") %>%
+  filter(site %in% wiki_proj) %>%
+  filter(class_name %in% taxa_groups) %>%
+  select(title, site, class_name) %>%
+  unique() %>%
+  mutate(site = factor(site, levels = wiki_proj)) %>%
+  arrange(site)
+
+# filter the biodiversity pages for the set of languages we're using, then split up, and sort by random languages list
+split_biodiversity_pages <- split(biodiversity_pages, biodiversity_pages$site)
+
+# filter all pages from random that are species pages
+filter_species <- function(data_file, split_biodiversity_pages){
+  data_fin <- anti_join(data_file, split_biodiversity_pages, by = c("article" = "title")) %>%
+    rename("wikipedia_id" = "q_wikidata")
+  return(data_fin)
+}
+
+# merge the random species title with the all species list for each language to remove species from random - antijoin to remove those in both
+total_random_views_2 <- list()
+for(i in 1:length(total_random_views)){
+  total_random_views_2[[i]] <- filter_species(data_file = total_random_views[[i]], 
+                                             split_biodiversity_pages = split_biodiversity_pages[[i]]) %>%
+    select(article, wikipedia_id, year, month, av_views, date)
+}
+
+# calculate the number of views without the random pages that are in the species trend
+calc_views <- function(data_file){
+  data_fin <- data_file %>%
+    summarise(total_views = sum(av_views))
+  return(data_fin)
+}
+
+# build new plot for the number of views after removing the random pages that are in the species trend
+views_remove_species <- lapply(total_random_views_2, calc_views) %>%
+  rbindlist() %>%
+  mutate(language = languages)
+
+# run function for each language views and build bar plot
+plot_views <- views_remove_species %>%
+  mutate(language = factor(language, levels = languages, 
+                           labels = c("Spanish", "French", "German", "Japanese", "Italian", 
+                                      "Arabic", "Russian", "Portuguese", "Chinese", "English"))) %>%
+  mutate(language = fct_reorder(language, -total_views)) %>%
+  ggplot() +
+  geom_bar(aes(x = language, y = total_views), stat = "identity") + 
+  geom_text(aes(x = language, y = total_views + 23000000, label = (round(total_views/1000000, digits = 2)))) +
+  xlab("Wiki project (language)") +
+  ylab("Total user views (millions)") +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 800000000), 
+                     breaks = c(0, 200000000, 400000000, 600000000, 800000000), labels = c("0", "200", "400", "600", "800")) +
+  theme_bw() +
+  theme(panel.grid = element_blank())
+
+# save plot for species removed from random trend
+ggsave("random_views_removed-species.png", dpi = 350, scale = 1)
