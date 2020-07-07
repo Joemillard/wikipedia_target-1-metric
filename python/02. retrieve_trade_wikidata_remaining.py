d244fe5bb4a284a7e250f5e1f1ded8230b8a5732
Joemillard <joeymillard@hotmail.co.uk>
2020-07-07 11:26:48 +0100

    commit scripts for pulling q_wikidata ids from api

---
+++ python/02. retrieve_trade_wikidata_remaining.py
@@ -0,0 +1,48 @@
+import requests
+import time
+import pandas as pd
+import numpy as np
+import json
+from pandas.io.json import json_normalize
+
+# read in the list of traded species
+traded_species = pd.read_csv("C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/traded_species_wikidata.csv")
+
+# remove all rows that are already id'd from english wikipedia, and then reset index
+traded_species = traded_species[traded_species['qwiki_id'].isnull()]
+traded_species.index = range(len(traded_species.index))
+print(traded_species)
+
+# select the traded species column
+traded_species = traded_species['title']                     
+
+# Get session for url query
+S = requests.Session()
+
+# write to result object
+result = []
+
+# request each species json data from the wiki api
+for i in range(0, len(traded_species)):
+ 
+    try:
+        #print(traded_species)
+        url = "https://fr.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=%s&format=json" % (traded_species[i])
+        R = requests.get(url = url)
+        DATA = R.json()
+        DATA = DATA['query']['pages']
+
+        DATA = json_normalize(DATA)
+        DATA.rename(columns = {DATA.columns[0]: "ns", DATA.columns[1]: "wikipedia_id",DATA.columns[2]: "qwiki_id",DATA.columns[3]: "title"}, inplace = True)
+
+        print(DATA)
+        result.append(DATA)
+    except IndexError:
+        DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
+        result.append(DATA)
+
+final = pd.concat(result)
+save_loc = 'C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/traded_species_wikidata_fr.csv'
+final.to_csv(save_loc, sep = ',', encoding = 'utf-8-sig')
+    
+    
