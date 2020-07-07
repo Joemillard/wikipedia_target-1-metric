7de9cd25e750a83e2f11209a1942ba8dc009f587
Joemillard <joeymillard@hotmail.co.uk>
2020-07-07 18:28:23 +0100

    additional script to pull wiki_id for each fish

--- python/02a. retrieve_fish_wikidata_remaining.py
+++ python/02a. retrieve_fish_wikidata_remaining.py
@@ -6,46 +6,103 @@ import json
 from pandas.io.json import json_normalize
 
 # read in the list of traded species
-traded_species = pd.read_csv("C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata.csv")
+# traded_species = pd.read_csv("C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata.csv")
+traded_species = pd.read_csv("C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata_other_es.csv")
 
-# remove all those that have already been identified
-traded_species = traded_species[traded_species['qwiki_id'].isnull()]
-traded_species.index = range(len(traded_species.index))
-print(traded_species)
+# try for each remaining language on API
+languages = ['ru', 'pt', 'it', 'ar', 'ja']
 
-# select the traded species column
-traded_species = traded_species['title']                     
+# iterate through each remaining language
+for l in range(0, len(languages)):
 
-# Get session for url query
-S = requests.Session()
+    if l > 0:
 
-# write to result object
-result = []
+        # read in the new version of fish data after downloading species from pervious set
+        read_loc = 'C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata_other_%s.csv' % (languages[l-1])
 
-# request each species json data from the wiki api
-for i in range(0, len(traded_species)):
-    try:
-        #print(traded_species)
-        url = "https://zh.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=%s&format=json" % (traded_species[i])
-        R = requests.get(url = url)
-        DATA = R.json()
-        DATA = DATA['query']['pages']
+        traded_species = pd.read_csv(read_loc)
 
-        DATA = json_normalize(DATA)
-        DATA.rename(columns = {DATA.columns[0]: "ns", DATA.columns[1]: "wikipedia_id",DATA.columns[2]: "qwiki_id",DATA.columns[3]: "title"}, inplace = True)
+        # remove all those that have already been identified
+        traded_species = traded_species[traded_species['qwiki_id'].isnull()]
+        traded_species.index = range(len(traded_species.index))
+        #print(final)
 
-        print(DATA)
-        result.append(DATA)
+        # select the traded species column
+        traded_species = traded_species['title']                     
 
-    except IndexError:
-        DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
-        result.append(DATA)
+        # Get session for url query
+        S = requests.Session()
 
-    except KeyError:
-        DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
-        result.append(DATA)
+        # write to result object
+        result = []
 
-final = pd.concat(result)
-save_loc = 'C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata_zh.csv'
-final.to_csv(save_loc, sep = ',', encoding = 'utf-8-sig')
+        # request each species json data from the wiki api
+        for i in range(0, len(traded_species)):
+            try:
+                #print(traded_species)
+                url = "https://%s.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=%s&format=json" % (languages[l], traded_species[i])
+                R = requests.get(url = url)
+                DATA = R.json()
+                DATA = DATA['query']['pages']
+
+                DATA = json_normalize(DATA)
+                DATA.rename(columns = {DATA.columns[0]: "ns", DATA.columns[1]: "wikipedia_id",DATA.columns[2]: "qwiki_id", DATA.columns[3]: "title"}, inplace = True)
+
+                print(DATA)
+                result.append(DATA)
+
+            except IndexError:
+                DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
+                result.append(DATA)
+
+            except KeyError:
+                DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
+                result.append(DATA)
+
+        final = pd.concat(result)
+        save_loc = 'C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata_other_%s.csv' % (languages[l])
+        final.to_csv(save_loc, sep = ',', encoding = 'utf-8-sig')
+
+    else: 
+    
+        # remove all those that have already been identified
+        traded_species = traded_species[traded_species['qwiki_id'].isnull()]
+        traded_species.index = range(len(traded_species.index))
+        print(traded_species)
+
+        # select the traded species column
+        traded_species = traded_species['title']                     
+
+        # Get session for url query
+        S = requests.Session()
+
+        # write to result object
+        result = []
+
+        # request each species json data from the wiki api
+        for i in range(0, len(traded_species)):
+            try:
+                #print(traded_species)
+                url = "https://%s.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=%s&format=json" % (languages[l], traded_species[i])
+                R = requests.get(url = url)
+                DATA = R.json()
+                DATA = DATA['query']['pages']
+
+                DATA = json_normalize(DATA)
+                DATA.rename(columns = {DATA.columns[0]: "ns", DATA.columns[1]: "wikipedia_id",DATA.columns[2]: "qwiki_id", DATA.columns[3]: "title"}, inplace = True)
+
+                print(DATA)
+                result.append(DATA)
+
+            except IndexError:
+                DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
+                result.append(DATA)
+
+            except KeyError:
+                DATA = pd.DataFrame({'ns':[''],'wikipedia_id':[''], 'qwiki_id':[''], 'title':[traded_species[i]]})
+                result.append(DATA)
+
+        final = pd.concat(result)
+        save_loc = 'C:/Users/joeym/Documents/PhD/Aims/Aim 3 - quantifying pollinator cultural value/wikipedia_target-1-metric/wikipedia_target-1-metric/data/class_wiki_indices/submission_2/fished_species_wikidata_other_%s.csv' % (languages[l])
+        final.to_csv(save_loc, sep = ',', encoding = 'utf-8-sig')
 
